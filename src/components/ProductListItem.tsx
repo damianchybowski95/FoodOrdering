921ffb48834e5bd603c5a3dfd52c18ac03998e1c
Damian Chybowski <damianchybowski@gmail.com>
2025-04-18 13:42:35 +0200

    typescript config

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,12 +1,15 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import Colors from "@/src/constants/Colors";
+import { Product } from "@/src/types";
 
-const ProductListItem = ({ product }) => {
+export const defaultPizzaImage = "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png"
+
+const ProductListItem = ({ product }: { product: Product }) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
+      <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} />
       <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>      
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 };
@@ -14,22 +17,22 @@ const ProductListItem = ({ product }) => {
 export default ProductListItem;
 
 const styles = StyleSheet.create({
-    container: {
-      backgroundColor: "white",
-      padding: 10,
-      borderRadius: 20,
-    },
-    image: {
-      width: "100%",
-      aspectRatio: 1,
-    },
-    title: {
-      fontSize: 18,
-      fontWeight: 600,
-      marginVertical: 10,
-    },
-    price: {
-      color: Colors.light.tint,
-      fontWeight: "bold",
-    },
-  });
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 600,
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold",
+  },
+});
